2c904bf1fe66c4963b0fa814d62c7fbb3a13d9e3
shivani3200 <shivani13222@gmail.com>
2024-12-15 02:30:26 +0530

    API and database schema

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import Connection from './database/db.js';
+import Router from './routes/route.js';
 
 const app =  express();
 
+app.use('/', Router);
 
 const PORT = 8000;
 
